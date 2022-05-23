62b7ffeab424419ec9909e85ba053ccf2eff5c28
jonasromano <88295040+jonasromano@users.noreply.github.com>
2022-05-23 19:15:21 -0300

    Archivos
    Todos los html, algunos JSON entre otros archivos

---
+++ Project port/explaboral.js
@@ -0,0 +1,9 @@
+var experiencia = [];
+
+function mostrarexperiencia(experiencia){
+    let exper = "";
+    for(let i= 0; i < experiencia.length; i++){
+        let ex = experiencia[i];
+        exper +=``
+    }
+}
